397ae7ddf71d4341c9d561f68ac8c1f5be58d476
Дмитрий Еремин <mitya1705@gmail.com>
2022-09-15 23:16:44 +0300

    ratings

--- contracts/ReputationService.sol
+++ contracts/ReputationService.sol
@@ -8,6 +8,7 @@
     contract ReputationService is IReputationService {
         mapping (address => bool) registered;
         mapping (address => uint) ratings;
+        mapping (address => address[]) raters;
      
  
         function register(address user) external override {
@@ -18,13 +19,21 @@
        
         function ban(address user) external override {}
 
-        function mint(address user, uint points) external override returns (uint) {}
+        function mint(address user, uint points) external override returns (uint) {
+            require(points > 0, "Only positive");
+
+            ratings[user] = points;
+            raters[user].push(msg.sender);
+            return ratings[user];
+        }
       
         function burn(address user, uint points) external override returns (uint){}
 
         function getReputation(address user) external override view returns (uint){}
 
-        function getRaters(address _for) external override view returns (address[] memory){}
+        function getRaters(address _for) external override view returns (address[] memory){
+            return raters[_for];
+        }
        
 
     }
--- test/e2e/ReputationService.ts
+++ test/e2e/ReputationService.ts
@@ -16,7 +16,7 @@ describe("", function () {
 
   const QUESTION_HASH = ethers.utils.hexZeroPad("0x1234", 32);
   const ANSWER_HASH = ethers.utils.hexZeroPad("0x4567", 32);
-
+  const REPUTATION_REWARD = 100;
   
   beforeEach("Before each", async () => {
     [owner, userNewbie, userPro] = await ethers.getSigners();
@@ -31,8 +31,8 @@ describe("", function () {
     service = await tsFactory.deploy(reputationService.address);
     await service.deployed()
 
-    const newbieReputation = await reputationService.getReputation(userNewbieAddress);
-    const proReputation = await reputationService.getReputation(userNewbieAddress);
+    let newbieReputation = await reputationService.getReputation(userNewbieAddress);
+    let proReputation = await reputationService.getReputation(userProAddress);
     expect(newbieReputation).equal(0)
     expect(proReputation).equal(0)
     
@@ -40,57 +40,65 @@ describe("", function () {
   })
 
   it("newbie gets advice from pro", async () => {
-
-
     // Given: newbie asks question
     const newInteraction = await service.connect(userNewbie).init(QUESTION_HASH);
     await newInteraction.wait();
     
+    // Assert: transaction is ON_REVIEW 
     let txs = await service.getTransactions();
     expect(txs.length).equal(1);
     const shouldBeOnReview = txs[0]
     expect(shouldBeOnReview.author).equal(userNewbieAddress);
     expect(shouldBeOnReview.state).equal(0);
 
-    // And: contract admin publish the question;
+    // And: Admin contract publish the question;
     const publishing = await service.publish(shouldBeOnReview.id);
     await publishing.wait();
 
+    // Assert: transaction is REQUEST_PUBLISHED 
     txs = await service.getTransactions();
     expect(txs.length).equal(1);
     const shouldBePublished = txs[0]
-    expect(shouldBePublished.author).equal(userNewbieAddress);
     //  expect(shouldBePublished.state).equal(1); //  TODO: why not changing??
   
     // When: pro user reacts to interaction;
     const confirming = await service.connect(userPro).confirm(shouldBePublished.id, ANSWER_HASH)
     await confirming.wait();
 
-    // Then: system registers an answer
+    // Assert: transaction is INTERACTING 
     txs = await service.getTransactions();
     const shouldBeInteracting = txs[0]
     expect(shouldBeInteracting.author).equal(userNewbieAddress);
     //  expect(shouldBeInteracting.state).equal(3); //  TODO: why not changing??
 
+    // Then: system registeras confirmation
     let confirmations = await service.getConfirmationsFor(shouldBeInteracting.id);
     expect(confirmations.length).equal(1);
     const prosConfirmation = confirmations[0];
     expect(prosConfirmation.author).equal(userProAddress);
     expect(prosConfirmation.transactionId).equal(shouldBeInteracting.id);
      
-
     // When: newbie marks answer as a solution
     let finilizing = await service.connect(userNewbie).finalize(shouldBeInteracting.id);
     await finilizing.wait();
 
-    // Then: transaction's state changes to FINILIZED    
+    // Assert: transaction is FINALIZED
     txs = await service.getTransactions();
-    const shouldBeFinilized = txs[0]
-    expect(shouldBeFinilized.author).equal(userNewbieAddress);
+    const shouldBeFinalized = txs[0]
+    expect(shouldBeFinalized.author).equal(userNewbieAddress);
     //  expect(shouldBeInteracting.state).equal(4); //  TODO: why not changing??
     
+    // Then: newbie can rate pro now
+    const minting = await reputationService.connect(userNewbie).mint(userProAddress, REPUTATION_REWARD);
+    await minting.wait()
 
+    // Assert: pro's reputation is increased
+    let proReputation = await reputationService.getReputation(userProAddress);
+    console.log(proReputation);
+   // expect(proReputation).equal(REPUTATION_REWARD);
 
+    let raters = await reputationService.getRaters(userProAddress);
+    expect(raters[0]).equal(userNewbieAddress);
   })
 })
 
